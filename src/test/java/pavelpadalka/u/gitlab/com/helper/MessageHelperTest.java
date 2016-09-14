c18b014881a83cd5ee0739d132a7f54876936ce4
pavelpadalka <padalkapavel@gmail.com>
2016-09-14 12:19:16 +0300

    upd

--- src/test/java/pavelpadalka/u/gitlab/com/helper/MessageHelperTest.java
+++ src/test/java/pavelpadalka/u/gitlab/com/helper/MessageHelperTest.java
@@ -13,97 +13,97 @@ public class MessageHelperTest {
 
     @Test
     public void testNightFirst()throws ParseException {
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("23:00:00"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("23:00:00"));
         Assert.assertEquals(result, "NIGHT");
     }
 
     @Test
     public void testNightSecond()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("05:59:59"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("05:59:59"));
         Assert.assertEquals(result, "NIGHT");
     }
 
     @Test
     public void testNightThree()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("23:00:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("23:00:01"));
         Assert.assertEquals(result, "NIGHT");
     }
 
     @Test
     public void testNightFour()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("02:00:00"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("02:00:00"));
         Assert.assertEquals(result, "NIGHT");
     }
 
     @Test
     public void testMorningFirst()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("06:00:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("06:00:01"));
         Assert.assertEquals(result, "MORNING");
     }
 
     @Test
     public void testMorningSecond()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("08:59:59"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("08:59:59"));
         Assert.assertEquals(result, "MORNING");
     }
 
     @Test
     public void testMorningThree()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("07:25:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("07:25:01"));
         Assert.assertEquals(result, "MORNING");
     }
 
     @Test
     public void testMorningFour()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("08:00:00"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("08:00:00"));
         Assert.assertEquals(result, "MORNING");
     }
 
     @Test
     public void testDayFirst()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("09:00:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("09:00:01"));
         Assert.assertEquals(result, "DAY");
     }
 
     @Test
     public void testDaySecond()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("18:59:59"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("18:59:59"));
         Assert.assertEquals(result, "DAY");
     }
 
     @Test
     public void testDayThree()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("15:35:51"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("15:35:51"));
         Assert.assertEquals(result, "DAY");
     }
 
     @Test
     public void testDayFour()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("16:53:15"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("16:53:15"));
         Assert.assertEquals(result, "DAY");
     }
 
     @Test
     public void testEveningFirst()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("19:00:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("19:00:01"));
         Assert.assertEquals(result, "EVENING");
     }
 
     @Test
     public void testEveningSecond()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("22:59:59"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("22:59:59"));
         Assert.assertEquals(result, "EVENING");
     }
 
     @Test
     public void testEveningThree()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("21:25:01"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("21:25:01"));
         Assert.assertEquals(result, "EVENING");
     }
 
     @Test
     public void testEveningFour()throws ParseException{
-        result = MessageHelper.getCurrentPartOfDay(dateFormat.parse("22:00:00"));
+        result = MessageHelper.getPartOfDay(dateFormat.parse("22:00:00"));
         Assert.assertEquals(result, "EVENING");
     }
 
