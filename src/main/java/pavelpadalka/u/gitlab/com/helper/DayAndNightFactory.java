37da1b0341adc8f8fd24e66a07ee3206875aa744
pavelpadalka <padalkapavel@gmail.com>
2016-09-20 09:24:31 +0300

    review

--- src/main/java/pavelpadalka/u/gitlab/com/helper/DayAndNightFactory.java
+++ src/main/java/pavelpadalka/u/gitlab/com/helper/DayAndNightFactory.java
@@ -10,23 +10,23 @@ import java.util.ResourceBundle;
 
 public class DayAndNightFactory {
 
-    private Date morningTimeLimit;
-    private Date dayTimeLimit;
-    private Date eveningTimeLimit;
-    private Date nightTimeLimit;
+    private static Date morningTimeLimit;
+    private static Date dayTimeLimit;
+    private static Date eveningTimeLimit;
+    private static Date nightTimeLimit;
     private static final Logger log = Logger.getLogger(DayAndNightFactory.class);
 
-    public DayAndNightFactory() {
+    static {
 
         ResourceBundle resourceBundle = ResourceBundle.getBundle("DayAndNightTimeLimits");
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
         try {
-            this.morningTimeLimit = dateFormat.parse(resourceBundle.getString("timeLimit.morning"));
-            this.dayTimeLimit     = dateFormat.parse(resourceBundle.getString("timeLimit.day"));
-            this.eveningTimeLimit = dateFormat.parse(resourceBundle.getString("timeLimit.evening"));
-            this.nightTimeLimit   = dateFormat.parse(resourceBundle.getString("timeLimit.night"));
+            morningTimeLimit = dateFormat.parse(resourceBundle.getString("timeLimit.morning"));
+            dayTimeLimit     = dateFormat.parse(resourceBundle.getString("timeLimit.day"));
+            eveningTimeLimit = dateFormat.parse(resourceBundle.getString("timeLimit.evening"));
+            nightTimeLimit   = dateFormat.parse(resourceBundle.getString("timeLimit.night"));
         } catch (ParseException e) {
             log.error("Parse of the time limit exception", e);
             System.exit(0);
@@ -34,44 +34,22 @@ public class DayAndNightFactory {
 
     }
 
-    public Date getTimeLimit(PartsOfDay partsOfDay) {
+    public static Date getTimeLimit(PartsOfDay partsOfDay) {
 
         Date result = null;
 
         if (partsOfDay.equals(PartsOfDay.MORNING)) {
-            result = this.morningTimeLimit;
+            result = morningTimeLimit;
         } else if (partsOfDay.equals(PartsOfDay.DAY)) {
-            result = this.dayTimeLimit;
+            result = dayTimeLimit;
         } else if (partsOfDay.equals(PartsOfDay.EVENING)) {
-            result = this.eveningTimeLimit;
+            result = eveningTimeLimit;
         } else if (partsOfDay.equals(PartsOfDay.NIGHT)) {
-            result = this.nightTimeLimit;
+            result = nightTimeLimit;
         }
 
             return result;
 
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        DayAndNightFactory that = (DayAndNightFactory) o;
-
-        if (!morningTimeLimit.equals(that.morningTimeLimit)) return false;
-        if (!dayTimeLimit.equals(that.dayTimeLimit)) return false;
-        if (!eveningTimeLimit.equals(that.eveningTimeLimit)) return false;
-        return nightTimeLimit.equals(that.nightTimeLimit);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = morningTimeLimit.hashCode();
-        result = 31 * result + dayTimeLimit.hashCode();
-        result = 31 * result + eveningTimeLimit.hashCode();
-        result = 31 * result + nightTimeLimit.hashCode();
-        return result;
-    }
 }
